77cc9907426da019476a7aed69c7092166ff4472
James D. Moffet <jim.moffet@gsa.gov>
2025-04-09 20:50:06 -0700

    integration testing

---
+++ src/.routes-upstream-overrides/(app)/c/[id]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { MAIN_CONTENT_ID as mainContentId } from '$lib/constants';
+
+	import Chat from '$lib/components/chat/Chat.svelte';
+	import Help from '$lib/components/layout/Help.svelte';
+</script>
+
+<Chat chatIdProp={$page.params.id} {mainContentId} />
